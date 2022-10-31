ec7fd5331a354647b407306a2ef6fede0e709d7a
akashram14 <aka2000_14@rediffmail.com>
2022-10-31 20:09:03 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	Token string
+)
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "start" {
+		for {
+			s.ChannelMessageSend(m.ChannelID, "Starting the tracker")
+		}
+	}
+
+	if m.Content == "stop" {
+		s.ChannelMessageSend(m.ChannelID, "Stopping the tracker!")
+	}
+}
+
+func main() {
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session", err)
+		return
+	}
+
+	dg.AddHandler(messageCreate)
+
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
+
+}
